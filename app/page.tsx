a2783c93678d2849695d9767c69e8f8d92cad2e8
Edmund Phillips <edmund.phillips@acity.edu.gh>
2024-08-19 20:23:28 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-center items-center">
+      <div className="flex justify-center items-center h-full w-full">
+        Blog Preview Component
+      </div>
+      
+    </main>
+  );
+}
